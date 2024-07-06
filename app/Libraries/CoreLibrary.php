bc234634e9c539fd0260d63f2d4bae900c97c535
Nicodemus Karisa <nkarisa@ke.ci.org>
2024-07-06 07:50:44 +0300

    Rename CustomfieldLibrary library casing

--- app/Libraries/CoreLibrary.php
+++ app/Libraries/CoreLibrary.php
@@ -196,7 +196,7 @@ class CoreLibrary
 
     protected function getAllowableResults($featureName)
     {
-        $pascalizeFeatureName = $featureName == 'custom_field' ? 'CustomField' : pascalize($featureName);
+        $pascalizeFeatureName = $featureName == 'customfield' ? 'CustomField' : pascalize($featureName);
         $featureLibary = new ('\\App\\Libraries\\' . $pascalizeFeatureName . 'Library')();
         return $featureLibary->{'getAllowable' . plural($pascalizeFeatureName)}();
     }
